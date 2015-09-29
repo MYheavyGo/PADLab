83c815160efe92618069e8d5e1e03848db5bea36
MYheavyGo <ga.sprunger@gmail.com>
2015-09-29 14:39:28 +0200

    Commit - Change the version
    Passer de Python 2.7 à 3.5 et utiliser l'API rtmidi2 -->
    https://github.com/gesellkammer/rtmidi2
    Enlever le fichier de recherche du Launchpad
    Modifier l'utilisation de l'ancienne par la nouvelle API

--- TestPADMIDI/Main.py
+++ TestPADMIDI/Main.py
@@ -1,25 +1,23 @@
 import sys
-import random
 import time
-import string
-from pygame import midi
-from Utils.launchpad_charset import CHARTAB
-from Utils.launchpad_device import get_devices
+import rtmidi2
+import numpy as np
 
-MIDI_BUFFER_OUT = 128
-MIDI_BUFFER_IN = 16
+MIDI_ON_LED = 128
+MIDI_OFF_LED = 144
 print(sys.version + "\n" + str(sys.api_version) + "\n" + str(sys.version_info) + "\n" + str(sys.getwindowsversion()))
 
 
 class ButtonTable:
     def __init__(self):
-        self.buttons = []
+        self.buttons = None
         self.init_buttons()
-        print(str(self.buttons) + "\ncount " + str(len(self.buttons)))
+        # print(str(self.buttons) + "\ncount " + str(len(self.buttons)))
 
     def init_buttons(self):
-        for x in xrange(0, 9):
-            for y in xrange(0, 9):
+        self.buttons = []
+        for x in range(0, 9):
+            for y in range(0, 9):
                 self.buttons.append([x, y, False])
 
     def get_cell(self, x, y):
@@ -42,72 +40,78 @@ class ButtonTable:
 class LaunchPAD:
 
     def __init__(self):
-        devices = get_devices()
-        print(devices)
+        self.midiOut = rtmidi2.MidiOut()
+        self.midiIn = rtmidi2.MidiIn()
         self.note = None
         self.pad = ButtonTable()
-        self.cell = None
-        if devices[2][3] > 0:
-            self.midiOut = midi.Output(devices[3], 0)
-        if devices[0][2] > 0:
-            self.midiIn = midi.Input(devices[1])
-
-    def read_check(self):
-        """Verifie si on peut recevoir des donnees depuis le PAD"""
-        return self.midiIn.poll()
+        self.midiOut.open_port(1)
+        self.midiIn.open_port(0)
 
     def led_all_on(self):
         """Allume toutes les LED"""
-        self.midiOut.write_short(176, 0, 127)
+        self.midiOut.send_noteon(176, 0, 127)
+
+    def led_all_off(self):
+        """Allume toutes les LED"""
+        self.midiOut.send_noteon(176, 0, 0)
 
     def test_write(self):
-        for column in xrange(0, 9):
-            for row in xrange(0, 8):
-                self.midiOut.write_short(144, (16 * row) + column, 15)
+        for column in range(0, 9):
+            for row in range(0, 8):
+                self.midiOut.send_noteon(MIDI_OFF_LED, (16 * row) + column, 15)
                 time.sleep(0.05)
         time.sleep(2)
 
-        for column in xrange(9, -1, -1):
-            for row in xrange(9, -1, -1):
-                self.midiOut.write_short(128, (16 * row) + column, 12)
+        for column in range(9, -1, -1):
+            for row in range(9, -1, -1):
+                self.midiOut.send_noteon(MIDI_ON_LED, (16 * row) + column, 12)
                 time.sleep(0.05)
         time.sleep(2)
+
+    def test_many_write(self):
+        notes = range(127)
+        velocities = 0
+        self.midiOut.send_noteon_many(MIDI_ON_LED, notes, velocities)
+        time.sleep(1)
+        self.midiOut.send_noteon_many(MIDI_ON_LED, notes, [0] * len(notes))
 
     def turn_on_on_press(self):
         """Allume la LED lorsqu'on presse dessus et aussi le sens contraire"""
-        if self.read_check():
-            self.note = self.midiIn.read(1)
-            # print(self.note)
-            if self.note[0][0][0] == 144 and self.note[0][0][2] > 0:
-                y = self.note[0][0][1] & 0x0f
-                x = (self.note[0][0][1] & 0xf0) >> 4
-                print("x --> " + str(x) + "\ny --> " + str(y) + "\npos --> " + str((y > 0 if x * y else x)))
+        if self.note is not None:
+            print(self.note)
+            time.sleep(0.05)
+            if self.note[0] == 144 and self.note[2] > 0:
+                y = self.note[1] & 0x0f
+                x = (self.note[1] & 0xf0) >> 4
+                print(self.pad.get_cell(x, y))
                 if self.pad.get_cell(x, y)[2]:
-                    self.cell = self.pad.set_cell(self.pad.get_cell(x, y), x, y, False)
-                    self.midiOut.write_short(128, self.note[0][0][1], 12)
+                    self.pad.set_cell(self.pad.get_cell(x, y), x, y, False)
+                    self.midiOut.send_noteon(128, self.note[1], 12)
                 else:
-                    self.cell = self.pad.set_cell(self.pad.get_cell(x, y), x, y, True)
-                    self.midiOut.write_short(144, self.note[0][0][1], 60)
-                print(self.cell)
-                time.sleep(0.05)
+                    self.pad.set_cell(self.pad.get_cell(x, y), x, y, True)
+                    self.midiOut.send_noteon(144, self.note[1], 60)
 
     def reset(self):
         """Methode qui reset le PAD en eteignanr toutes les LEDs"""
-        self.midiOut.write_short(176, 0, 0)
+        self.midiOut.send_noteon(176, 0, 0)
 
 
 def main():
     """Methode main qui permet d'initialiser l'API MIDI ainsi que le LaunchPAD"""
-    midi.init()
     launchpad = LaunchPAD()
-    # pad.led_all_on()
-    # pad.test_write()
-    while 1:
-        launchpad.turn_on_on_press()
-        if launchpad.note is not None:
-            if launchpad.note[0][0][1] == 120:
+    launchpad.led_all_on()
+    time.sleep(0.1)
+    launchpad.led_all_off()
+    launchpad.test_many_write()
+    """while True:
+        message, delta_time = launchpad.midiIn.get_message()
+        if message:
+            launchpad.note = message
+            # print(message, delta_time)
+            if message[1] == 104:
                 break
+            else:
+                launchpad.turn_on_on_press()"""
     launchpad.reset()
-    midi.quit()
 
 main()
