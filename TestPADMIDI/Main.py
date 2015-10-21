6f868fa8c4d2ba169b0f3238c9e18b0359c38f4b
MYheavyGo <ga.sprunger@gmail.com>
2015-10-21 11:59:35 +0200

    Commit - 21.10.2015
    Correction de l'allumage des boutons
    - Array Numpy
    - Callback
    - Arrêt du script lorsque le PAD n'est pas connecté
    - Modification de nom de fonction
    - Allumage fonctionnel
    Ajout d'une fenêtre avec Pyglet
    Ajout d'une version

--- TestPADMIDI/Main.py
+++ TestPADMIDI/Main.py
@@ -1,72 +1,79 @@
 import sys
 import time
 import rtmidi_python as rtmidi
-# import numpy as np
+import numpy as np
+import pyglet
 
-MIDI_ON_LED = 128
-MIDI_OFF_LED = 144
+VERSION = '0.0.1'
+MIDI_ON_LED = 144
+MIDI_OFF_LED = 128
 MIDI_GREEN = 0
 MIDI_RED = 0
 INDEX_COLOR = [0, 1, 2, 3]
-print(sys.version + "\n" + str(sys.api_version) + "\n" + str(sys.version_info) + "\n" + str(sys.getwindowsversion()))
+
+print(sys.version, str(sys.api_version), str(sys.version_info), str(sys.getwindowsversion()), sep='\n')
+print("Version du programme ", VERSION, sep=': ')
+
+window = pyglet.window.Window()
 
 
 class ColorList:
     def __init__(self):
-        self.colors = None
-        self.init_color()
-        # print(self.colors)
-
-    def init_color(self):
-        self.colors = []
-        for r in range(0, 4, 3):
-            for g in range(0, 4, 3):
-                self.colors.append((r, g))
+        self.colors = np.array([(0, 0), (0, 3), (3, 0), (3, 3)])
 
 
 class ButtonTable:
     def __init__(self):
-        self.buttons = None
-        self.init_buttons()
-        # print(str(self.buttons) + "\ncount " + str(len(self.buttons)))
-
-    def init_buttons(self):
-        self.buttons = []
-        for x in range(0, 9):
-            for y in range(0, 9):
-                self.buttons.append([x, y, False])
+        self.buttons = np.zeros((8, 9), dtype=bool)
 
     def get_cell(self, x, y):
-        if y > 0:
-            pos = x * y
-        else:
-            pos = x
-        return self.buttons[pos]
+        return self.buttons[x][y]
 
-    def set_cell(self, info, x, y, click):
-        if y > 0:
-            pos = x * y
-        else:
-            pos = x
-        info[2] = click
-        self.buttons[pos] = info
-        return self.buttons[pos]
+    def set_cell(self, x, y, click):
+        self.buttons[x][y] = click
 
 
 class LaunchPAD:
 
     def __init__(self):
+        self.quit = False
         self.midiOut = rtmidi.MidiOut()
         self.midiIn = rtmidi.MidiIn()
         self.note = None
         self.color = None
         self.pad = ButtonTable()
         self.rgb = ColorList()
-        self.midiOut.open_port(1)
-        self.midiIn.open_port(0)
-        # print(self.rgb.colors)
 
-    def get_led_color(self, red, green):
+        if len(self.midiOut.ports) > 1:
+            self.midiOut.open_port(1)
+        else:
+            sys.exit("Pas d'appareil MIDI trouvé")
+
+        if len(self.midiIn.ports) > 0:
+            self.midiIn.callback = self.callback
+            self.midiIn.open_port(0)
+        else:
+            sys.exit("Pas d'appareil MIDI trouvé")
+
+    def callback(self, message, time_stamp):
+        if message:
+            self.note = message
+            print(self.note)
+            if message[0] == 176 and message[1] == 104:
+                self.quit = True
+            elif message[1] == 105:
+                self.clear()
+            elif message[1] == 106:
+                self.color = INDEX_COLOR[1]
+            elif message[1] == 107:
+                self.color = INDEX_COLOR[2]
+            elif message[1] == 108:
+                self.color = INDEX_COLOR[3]
+            else:
+                self.turn_on_on_press(self.color)
+
+    @staticmethod
+    def get_led_color(red, green):
         led = 0
 
         red = min(int(red), 3)
@@ -82,90 +89,76 @@ class LaunchPAD:
 
     def led_all_on(self):
         """Allume toutes les LED"""
-        self.midiOut.send_noteon(176, 0, 127)
+        self.midiOut.send_message([176, 0, 127])
 
-    def led_all_off(self):
+    def clear(self):
         """Allume toutes les LED"""
-        self.midiOut.send_noteon(176, 0, 0)
+        self.midiOut.send_message([176, 0, 0])
+        self.pad.buttons = np.zeros((8, 9), dtype=bool)
 
     def test_write(self):
         for row in range(0, 8):
             for column in range(0, 9):
                 color = self.get_led_color(self.rgb.colors[0][0], self.rgb.colors[0][1])
-                self.midiOut.send_noteon(MIDI_OFF_LED, (16 * row) + column, color)
+                self.midiOut.send_message(MIDI_OFF_LED, (16 * row) + column, color)
                 time.sleep(0.05)
         time.sleep(2)
 
         color = self.get_led_color(0, 0)
         for column in range(9, -1, -1):
             for row in range(9, -1, -1):
-                self.midiOut.send_noteon(MIDI_ON_LED, (16 * row) + column, color)
+                self.midiOut.send_message(MIDI_ON_LED, (16 * row) + column, color)
                 time.sleep(0.05)
         time.sleep(2)
 
     def turn_on_on_press(self, index=1):
-        """Allume la LED lorsqu'on presse dessus et aussi le sens contraire"""
-        if self.note is not None:
-            # print(self.note)
-            time.sleep(0.05)
+        """Allume la LED lorsqu'on presse dessus et l'eteint lorsqu'on reappuie"""
+        color = self.get_led_color(self.rgb.colors[index][0], self.rgb.colors[index][1])
+        if self.note[0] == 144 and self.note[2] == 127:
+            x = self.note[1] // 16
+            y = self.note[1] - (16 * x)
+
+            # self.midiOut.send_message([MIDI_ON_LED, self.note[1], color])
             color = self.get_led_color(self.rgb.colors[index][0], self.rgb.colors[index][1])
-            if self.note[0] == 144 and self.note[2] > 0:
-                y = self.note[1] & 0x0f
-                x = (self.note[1] & 0xf0) >> 4
-                # print(self.pad.get_cell(x, y))
-                if self.pad.get_cell(x, y)[2]:
-                    self.pad.set_cell(self.pad.get_cell(x, y), x, y, False)
-                    self.midiOut.send_message([128, self.note[1], color])
-                else:
-                    color = 0
-                    self.pad.set_cell(self.pad.get_cell(x, y), x, y, True)
-                    self.midiOut.send_message([144, self.note[1], color])
+
+            if not self.pad.get_cell(x, y):
+                print("Allume")
+                self.pad.set_cell(x, y, True)
+                self.midiOut.send_message([MIDI_ON_LED, self.note[1], color])
+            else:
+                print("Eteint")
+                color = 0
+                self.pad.set_cell(x, y, False)
+                self.midiOut.send_message([MIDI_OFF_LED, self.note[1], color])
 
     def reset(self):
         """Methode qui reset le PAD en eteignanr toutes les LEDs"""
         self.midiOut.send_message([176, 0, 0])
+        self.midiIn.close_port()
+        self.midiOut.close_port()
 
 
-def init(pad):
-    pad.color = 2
-    pad.midiOut.send_message([MIDI_ON_LED, 104, pad.get_led_color(pad.rgb.colors[pad.color][0], pad.rgb.colors[pad.color][1])])
+def init(windows, pad):
+    print(windows, pad)
     pass
 
 
-def main(INDEX_COLOR=None):
+def main():
     """Methode main qui permet d'initialiser l'API MIDI ainsi que le LaunchPAD"""
-    mdid = rtmidi.MidiOut()
-
     launchpad = LaunchPAD()
     launchpad.color = INDEX_COLOR[1]
 
-    init(launchpad)
+    init(window, launchpad)
 
-    # launchpad.led_all_on()
-    # time.sleep(0.1)
-    # launchpad.led_all_off()
-    # launchpad.test_write()
+    pyglet.app.run()
 
-    while True:
-        message, delta_time = launchpad.midiIn.get_message()
-        if message:
-            print(message)
-            launchpad.note = message
-            # print(message, delta_time)
+    @window.event
+    def on_draw():
+        window.clear()
 
-            if message[1] == 104:
-                break
-            elif message[1] == 105:
-                launchpad.reset()
-            elif message[1] == 106:
-                launchpad.color = INDEX_COLOR[1]
-            elif message[1] == 107:
-                launchpad.color = INDEX_COLOR[2]
-            elif message[1] == 108:
-                launchpad.color = INDEX_COLOR[3]
-            else:
-                launchpad.turn_on_on_press(launchpad.color)
-    launchpad.reset()
-    del launchpad
+    @window.event
+    def on_exit():
+        launchpad.reset()
+        del launchpad
 
-main(INDEX_COLOR=INDEX_COLOR)
+main()
