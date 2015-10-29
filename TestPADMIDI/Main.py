217486db7b24e5d26a7a4cf4096d9f11341442fd
MYheavyGo <ga.sprunger@gmail.com>
2015-10-29 15:58:55 +0100

    Commit - Use button to move

--- TestPADMIDI/Main.py
+++ TestPADMIDI/Main.py
@@ -1,15 +1,16 @@
 # Python / Import
 from __future__ import division
+
 import sys
-from random import randint
 # rtmidi / Import
 import rtmidi_python as rtmidi
 # Numpy / Import
 import numpy as np
 # Kivy / Import
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty, ListProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
 
@@ -18,7 +19,7 @@ TITLE = "Game / Porte ouverte CEFF"
 DESCRIPTION = "Game with the Kivy and a LaunchPAD for the controller"
 
 # Constantes OpenGL
-WIDTH = 1280
+WIDTH = 800
 HEIGHT = 720
 
 # Constantes pour le launchPAD
@@ -27,9 +28,12 @@ MIDI_OFF_LED = 128
 MIDI_AUTOMAP_ON = 176
 MIDI_GREEN = 0
 MIDI_RED = 0
-DICO_COLOR = {"White": (0,0) , "Red_FULL": (3, 0), "Red_LOW": (1, 0), "Green_FULL": (0, 3), "Green_LOW": (0, 1), "Yellow_FULL": (3, 3)}
+DICO_COLOR = {"White": (0, 0), "Red_FULL": (3, 0), "Red_LOW": (1, 0), "Green_FULL": (0, 3), "Green_LOW": (0, 1),
+              "Yellow_FULL": (3, 3)}
 
-print(sys.version, str(sys.api_version), str(sys.version_info), str(sys.getwindowsversion()), sep='\n')
+isPressed = False
+
+print(sys.version, str(sys.api_version), str(sys.version_info), str(sys.getwindowsversion()), sep=' // ')
 print("Version du programme ", VERSION, sep=': ')
 
 
@@ -82,41 +86,41 @@ class LaunchPAD:
         self.baseColor = colorDefault
 
     def callback(self, message, time_stamp):
+        global isPressed
         if message:
             self.note = message
-            if message[0] == 176 and message[1] == 104:
-                pass
-                # self.quit = True
-            elif message[1] == 105 and message[2] == 127:
-                self.clear()
-            elif message[1] == 106:
-                self.baseColor = DICO_COLOR["Green_FULL"]
-            elif message[1] == 107:
-                self.baseColor = DICO_COLOR["Red_FULL"]
-            elif message[1] == 108:
-                self.baseColor = DICO_COLOR["Yellow_FULL"]
-            else:
-                self.turn_on_on_press(self.baseColor)
-                pass
+            if message[0] == 144:
+                if message[2] == 127:
+                    isPressed = True
+                elif message[2] == 0:
+                    isPressed = False
+            # if message[0] == 176 and message[1] == 104:
+            #     pass
+            #     # self.quit = True
+            # elif message[1] == 105 and message[2] == 127:
+            #     self.clear()
+            # elif message[1] == 106:
+            #     self.baseColor = DICO_COLOR["Green_FULL"]
+            # elif message[1] == 107:
+            #     self.baseColor = DICO_COLOR["Red_FULL"]
+            # elif message[1] == 108:
+            #     self.baseColor = DICO_COLOR["Yellow_FULL"]
+            # else:
+            #     self.turn_on_on_press()
+            #     pass
 
     @staticmethod
     def get_led_color(red, green):
         led = 0
 
         red = min(int(red), 3)
-        print("Rouge #1 -> ", red)
         red = max(red, 0)
-        print("Rouge #2 -> ", red)
 
         green = min(int(green), 3)
-        print("Vert #1 -> ", green)
         green = max(green, 0)
-        print("Vert #2 -> ", green)
 
         led |= red
-        print("Led -> ", led)
         led |= green << 4
-        print("Led -> ", led)
 
         return led
 
@@ -127,27 +131,31 @@ class LaunchPAD:
     def clear(self):
         """Allume toutes les LED"""
         self.midiOut.send_message([176, 0, 0])
-        self.pad.buttons = np.zeros((8, 9), dtype=bool)
-        self.init(self.color)
+        self.pad.buttons = np.array([[Button(x=x, y=y) for y in range(9)] for x in range(8)])
+        self.init(self.baseColor)
 
-    def turn_on_on_press(self, index=1):
+    def turn_on_on_press(self):
         """Allume la LED lorsqu'on presse dessus et l'eteint lorsqu'on reappuie"""
         if self.note[0] == 144 and self.note[2] == 127:
             x = self.note[1] // 16
             y = self.note[1] - (16 * x)
 
-            # self.midiOut.send_message([MIDI_ON_LED, self.note[1], color])
-            color = self.get_led_color([index][0], self.rgb.colors[index][1])
+            changeColor = False
+            button = self.pad.get_cell(x, y)
+            color = self.get_led_color(self.baseColor[0], self.baseColor[1])
 
-            if not self.pad.get_cell(x, y).isPressed:
+            if button.color is not color:
+                changeColor = True
+
+            if not button.isPressed or changeColor:
                 self.pad.set_cell(x, y, True, color)
                 self.midiOut.send_message([MIDI_ON_LED, self.note[1], color])
-            else:
-                self.pad.set_cell(x, y, False, self.rgb.colors[0])
+            elif button.isPressed:
+                self.pad.set_cell(x, y, False, color)
                 self.midiOut.send_message([MIDI_OFF_LED, self.note[1], color])
 
     def reset(self):
-        """Methode qui reset le PAD en eteignanr toutes les LEDs"""
+        """Methode qui d'arrêt complet du PAD"""
         self.midiOut.send_message([176, 0, 0])
         self.midiIn.close_port()
         self.midiOut.close_port()
@@ -155,6 +163,7 @@ class LaunchPAD:
 
 class Paddle(Widget):
     score = NumericProperty(0)
+    posPaddle = ListProperty([])
 
     def bounce_ball(self, ball):
         if self.collide_widget(ball):
@@ -162,13 +171,33 @@ class Paddle(Widget):
                 vx, vy = ball.velocity
                 offset = (ball.center_y - self.center_y) / (self.height / 2)
                 bounced = Vector(-1 * vx, vy)
-                vel = bounced * 1.1
-                ball.velocity = vel.x, vel.y + offset
+                if bounced.x >= ball.velocity_max[0] or bounced.x <= ball.velocity_max[1]:
+                    pass
+                else:
+                    if bounced.x > 0:
+                        bounced.x += ball.velocity_up
+                    else:
+                        bounced.x -= ball.velocity_up
+                ball.velocity = bounced.x, bounced.y + offset
+
+    def init_move(self):
+        nbrY = 8
+        nbrX = 8
+
+        pos_x = Window.width // nbrX
+        pos_y = Window.height // nbrY
+        for i in range(nbrX):
+            for j in range(nbrY):
+                self.posPaddle.append((pos_x * i, Window.width - pos_y * j))
+        print(self.posPaddle)
 
 
 class Ball(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
+
+    velocity_max = (30, -30)
+    velocity_up = 0.5
 
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
@@ -183,10 +212,13 @@ class Game(Widget):
 
     launchpad = LaunchPAD()
     launchpad.init(DICO_COLOR["Green_FULL"])
-    launchpad.midiOut.send_message([MIDI_ON_LED, 32, 51])
-    launchpad.midiOut.send_message([MIDI_ON_LED, 80, 51])
-    launchpad.midiOut.send_message([MIDI_ON_LED, 39, 51])
-    launchpad.midiOut.send_message([MIDI_ON_LED, 87, 51])
+
+    firstTime = True
+
+    # launchpad.midiOut.send_message([MIDI_ON_LED, 32, 51])
+    # launchpad.midiOut.send_message([MIDI_ON_LED, 80, 51])
+    # launchpad.midiOut.send_message([MIDI_ON_LED, 39, 51])
+    # launchpad.midiOut.send_message([MIDI_ON_LED, 87, 51])
 
     def serve_ball(self, vel=(4, 0)):
         self.ball.center = self.center
@@ -195,6 +227,14 @@ class Game(Widget):
     def update(self, dt):
         self.ball.move()
 
+        if Window.width >= WIDTH and self.firstTime:
+            self.player1.init_move()
+            self.player2.init_move()
+            self.firstTime = False
+
+        if isPressed:
+            self.move_paddle(self.launchpad.note, self.player1, self.player2)
+
         self.player1.bounce_ball(self.ball)
         self.player2.bounce_ball(self.ball)
 
@@ -202,18 +242,32 @@ class Game(Widget):
             self.ball.velocity_y *= -1
 
         if self.ball.x < self.x:
-            print("Point Player 2")
             self.player2.score += 1
             self.serve_ball(vel=(4, 0))
         if self.ball.x > self.width:
-            print("Point Player 1")
             self.player1.score += 1
             self.serve_ball(vel=(-4, 0))
+
+    def move_paddle(self, message, player1, player2):
+        posNot = [(3, 4), (19, 20), (35, 36), (51, 52), (67, 68), (83, 84), (99, 100), (115, 116)]
+
+        x = message[1] // 16
+        y = message[1] - (16 * x)
+
+        if y < posNot[x][0]:
+            player1.x = player1.posPaddle[y][1]
+            player1.y = player1.posPaddle[y][0]
+        elif y > posNot[x][1]:
+            player2.x = player1.posPaddle[y][1]
+            player2.y = player1.posPaddle[y][0]
+        else:
+            print("Don't move")
 
 
 class MainApp(App):
     def build(self):
         self.title = TITLE
+        Window.fullscreen = "auto"
         game = Game()
         game.serve_ball()
         Clock.schedule_interval(game.update, 1.0 / 60.0)
