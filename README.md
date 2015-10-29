bdefd213f446f3bb29f56c8e503a5725524a5b53
MYheavyGo <ga.sprunger@gmail.com>
2015-10-29 12:46:14 +0100

    Commit - Correct bug with color

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Game written in Python and use the Launchpad MINI
 
 # Python
-  - Use 3.5
-  - Pycharm 4.5.4
+  - Use Python 3.5
+  - Pycharm 5.0 EAP / Build : 143.414
   - rtmidi / Lien : http://www.lfd.uci.edu/~gohlke/pythonlibs/#rtmidi-python - Version : rtmidi_python 0.2.2 x64 for 3.5
   - Launchpad MINI
+  - Kivy 1.9.0
--- TestPADMIDI/Main.py
+++ TestPADMIDI/Main.py
@@ -1,48 +1,64 @@
+# Python / Import
+from __future__ import division
 import sys
-import time
+from random import randint
+# rtmidi / Import
 import rtmidi_python as rtmidi
+# Numpy / Import
 import numpy as np
-import pyglet
+# Kivy / Import
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
 
 VERSION = '0.0.1'
+TITLE = "Game / Porte ouverte CEFF"
+DESCRIPTION = "Game with the Kivy and a LaunchPAD for the controller"
+
+# Constantes OpenGL
+WIDTH = 1280
+HEIGHT = 720
+
+# Constantes pour le launchPAD
 MIDI_ON_LED = 144
 MIDI_OFF_LED = 128
+MIDI_AUTOMAP_ON = 176
 MIDI_GREEN = 0
 MIDI_RED = 0
-INDEX_COLOR = [0, 1, 2, 3]
+DICO_COLOR = {"White": (0,0) , "Red_FULL": (3, 0), "Red_LOW": (1, 0), "Green_FULL": (0, 3), "Green_LOW": (0, 1), "Yellow_FULL": (3, 3)}
 
 print(sys.version, str(sys.api_version), str(sys.version_info), str(sys.getwindowsversion()), sep='\n')
 print("Version du programme ", VERSION, sep=': ')
 
-window = pyglet.window.Window()
 
-
-class ColorList:
-    def __init__(self):
-        self.colors = np.array([(0, 0), (0, 3), (3, 0), (3, 3)])
+class Button:
+    def __init__(self, x, y):
+        self.isPressed = False
+        self.color = None
+        self.pos = (x, y)
 
 
 class ButtonTable:
     def __init__(self):
-        self.buttons = np.zeros((8, 9), dtype=bool)
+        self.buttons = np.array([[Button(x=x, y=y) for y in range(9)] for x in range(8)])
 
     def get_cell(self, x, y):
         return self.buttons[x][y]
 
-    def set_cell(self, x, y, click):
-        self.buttons[x][y] = click
+    def set_cell(self, x, y, click, color):
+        self.buttons[x][y].color = color
+        self.buttons[x][y].isPressed = click
 
 
 class LaunchPAD:
-
     def __init__(self):
-        self.quit = False
         self.midiOut = rtmidi.MidiOut()
         self.midiIn = rtmidi.MidiIn()
         self.note = None
-        self.color = None
         self.pad = ButtonTable()
-        self.rgb = ColorList()
+        self.baseColor = None
 
         if len(self.midiOut.ports) > 1:
             self.midiOut.open_port(1)
@@ -55,35 +71,52 @@ class LaunchPAD:
         else:
             sys.exit("Pas d'appareil MIDI trouvé")
 
+    def init(self, colorDefault):
+        self.baseColor = self.get_led_color(DICO_COLOR["Green_FULL"][0], DICO_COLOR["Green_FULL"][1])
+        self.midiOut.send_message([MIDI_AUTOMAP_ON, 106, self.baseColor])
+        self.baseColor = self.get_led_color(DICO_COLOR["Red_FULL"][0], DICO_COLOR["Red_FULL"][1])
+        self.midiOut.send_message([MIDI_AUTOMAP_ON, 107, self.baseColor])
+        self.baseColor = self.get_led_color(DICO_COLOR["Yellow_FULL"][0], DICO_COLOR["Yellow_FULL"][1])
+        self.midiOut.send_message([MIDI_AUTOMAP_ON, 108, self.baseColor])
+
+        self.baseColor = colorDefault
+
     def callback(self, message, time_stamp):
         if message:
             self.note = message
-            print(self.note)
             if message[0] == 176 and message[1] == 104:
-                self.quit = True
-            elif message[1] == 105:
+                pass
+                # self.quit = True
+            elif message[1] == 105 and message[2] == 127:
                 self.clear()
             elif message[1] == 106:
-                self.color = INDEX_COLOR[1]
+                self.baseColor = DICO_COLOR["Green_FULL"]
             elif message[1] == 107:
-                self.color = INDEX_COLOR[2]
+                self.baseColor = DICO_COLOR["Red_FULL"]
             elif message[1] == 108:
-                self.color = INDEX_COLOR[3]
+                self.baseColor = DICO_COLOR["Yellow_FULL"]
             else:
-                self.turn_on_on_press(self.color)
+                self.turn_on_on_press(self.baseColor)
+                pass
 
     @staticmethod
     def get_led_color(red, green):
         led = 0
 
         red = min(int(red), 3)
+        print("Rouge #1 -> ", red)
         red = max(red, 0)
+        print("Rouge #2 -> ", red)
 
         green = min(int(green), 3)
+        print("Vert #1 -> ", green)
         green = max(green, 0)
+        print("Vert #2 -> ", green)
 
         led |= red
+        print("Led -> ", led)
         led |= green << 4
+        print("Led -> ", led)
 
         return led
 
@@ -95,40 +128,22 @@ class LaunchPAD:
         """Allume toutes les LED"""
         self.midiOut.send_message([176, 0, 0])
         self.pad.buttons = np.zeros((8, 9), dtype=bool)
-
-    def test_write(self):
-        for row in range(0, 8):
-            for column in range(0, 9):
-                color = self.get_led_color(self.rgb.colors[0][0], self.rgb.colors[0][1])
-                self.midiOut.send_message(MIDI_OFF_LED, (16 * row) + column, color)
-                time.sleep(0.05)
-        time.sleep(2)
-
-        color = self.get_led_color(0, 0)
-        for column in range(9, -1, -1):
-            for row in range(9, -1, -1):
-                self.midiOut.send_message(MIDI_ON_LED, (16 * row) + column, color)
-                time.sleep(0.05)
-        time.sleep(2)
+        self.init(self.color)
 
     def turn_on_on_press(self, index=1):
         """Allume la LED lorsqu'on presse dessus et l'eteint lorsqu'on reappuie"""
-        color = self.get_led_color(self.rgb.colors[index][0], self.rgb.colors[index][1])
         if self.note[0] == 144 and self.note[2] == 127:
             x = self.note[1] // 16
             y = self.note[1] - (16 * x)
 
             # self.midiOut.send_message([MIDI_ON_LED, self.note[1], color])
-            color = self.get_led_color(self.rgb.colors[index][0], self.rgb.colors[index][1])
+            color = self.get_led_color([index][0], self.rgb.colors[index][1])
 
-            if not self.pad.get_cell(x, y):
-                print("Allume")
-                self.pad.set_cell(x, y, True)
+            if not self.pad.get_cell(x, y).isPressed:
+                self.pad.set_cell(x, y, True, color)
                 self.midiOut.send_message([MIDI_ON_LED, self.note[1], color])
             else:
-                print("Eteint")
-                color = 0
-                self.pad.set_cell(x, y, False)
+                self.pad.set_cell(x, y, False, self.rgb.colors[0])
                 self.midiOut.send_message([MIDI_OFF_LED, self.note[1], color])
 
     def reset(self):
@@ -138,27 +153,77 @@ class LaunchPAD:
         self.midiOut.close_port()
 
 
-def init(windows, pad):
-    print(windows, pad)
-    pass
+class Paddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            if self.collide_widget(ball):
+                vx, vy = ball.velocity
+                offset = (ball.center_y - self.center_y) / (self.height / 2)
+                bounced = Vector(-1 * vx, vy)
+                vel = bounced * 1.1
+                ball.velocity = vel.x, vel.y + offset
+
+
+class Ball(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+
+class Game(Widget):
+    ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
+
+    launchpad = LaunchPAD()
+    launchpad.init(DICO_COLOR["Green_FULL"])
+    launchpad.midiOut.send_message([MIDI_ON_LED, 32, 51])
+    launchpad.midiOut.send_message([MIDI_ON_LED, 80, 51])
+    launchpad.midiOut.send_message([MIDI_ON_LED, 39, 51])
+    launchpad.midiOut.send_message([MIDI_ON_LED, 87, 51])
+
+    def serve_ball(self, vel=(4, 0)):
+        self.ball.center = self.center
+        self.ball.velocity = vel
+
+    def update(self, dt):
+        self.ball.move()
+
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+
+        if self.ball.y < 0 or self.ball.top > self.height:
+            self.ball.velocity_y *= -1
+
+        if self.ball.x < self.x:
+            print("Point Player 2")
+            self.player2.score += 1
+            self.serve_ball(vel=(4, 0))
+        if self.ball.x > self.width:
+            print("Point Player 1")
+            self.player1.score += 1
+            self.serve_ball(vel=(-4, 0))
+
+
+class MainApp(App):
+    def build(self):
+        self.title = TITLE
+        game = Game()
+        game.serve_ball()
+        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        return game
 
 
 def main():
     """Methode main qui permet d'initialiser l'API MIDI ainsi que le LaunchPAD"""
-    launchpad = LaunchPAD()
-    launchpad.color = INDEX_COLOR[1]
+    window = MainApp()
+    window.run()
 
-    init(window, launchpad)
-
-    pyglet.app.run()
-
-    @window.event
-    def on_draw():
-        window.clear()
-
-    @window.event
-    def on_exit():
-        launchpad.reset()
-        del launchpad
 
 main()
