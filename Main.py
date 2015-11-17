b8567d6b52dc3107fca7c798b76dc71b1831d010
MYheavyGo <ga.sprunger@gmail.com>
2015-11-17 16:40:40 +0100

    Finish the version 0.2.0
    Fixes a lot of bugs
    Features
    - Add collision (My collision because the framework is not a game engine
    is just a graphics engine)
    - Add position and  move the paddle with the LaunchPAD
    - think how to add level in game

--- Main.py
+++ Main.py
@@ -1,19 +1,17 @@
 # import Python
-from random import randint
+from random import randint, random
 import sys
-
+import math
 # import MIDI
-import rtmidi_python
-
+import rtmidi_python as rtmidi
 # import Pyglet
-import select
+from builtins import print
 from pyglet import font, clock, app, resource as rs
 from pyglet.window import key
 from pyglet.media import Player, SourceGroup
 from pyglet.text import *
 from pyglet.graphics import *
 from pyglet.sprite import *
-
 # import Numpy
 import numpy as np
 
@@ -33,89 +31,213 @@ ball = rs.image('ball.png')
 icon = pyglet.image.load('icon.png')
 music = pyglet.resource.media('music_background.mp3', False)
 
-# Score
-player1_score = Label('0', x=0, y=0)
-player2_score = Label('0', x=0, y=0)
 
 # Class for the Game
 class Player(Sprite):
-    def __init__(self, LEFTorRIGHT, img, x, y, batch):
-        super(Player, self).__init__(img, x, y, batch=batch)
+    def __init__(self, LEFTorRIGHT, img, x, y, batch, window):
+        super(Player, self).__init__(img=img, x=x, y=y, batch=batch)
         self.NUMBER = LEFTorRIGHT
         self.isPressed = False
         self.colors = None
         self.score = 0
+        self.window = window
+
+        self.positions = []
+        self.init_pos()
+
+    def init_pos(self):
+        nbrX = 8
+        nbrY = 8
+
+        interval_x = self.window.width // nbrX
+        interval_y = self.window.height // nbrY
+
+        for i in range(nbrY):
+            for j in range(nbrX // 2):
+                if self.NUMBER == 0:
+                    self.positions.append((j * interval_x, self.window.height - (i * interval_y) - paddle.height if self.window.height - (i * interval_y) - paddle.height > 0 else 0))
+                if self.NUMBER == 1:
+                    self.positions.append((self.window.width - (j * interval_x) - 30, self.window.height - (i * interval_y) - paddle.height if self.window.height - (i * interval_y) - paddle.height > 0 else 0))
+
 
 class Ball(Sprite):
     def __init__(self, img, x, y, batch):
         super(Ball, self).__init__(img=img, x=x, y=y, batch=batch)
-        self.dx = 15
-        self.dy = randint(-25, 25)
+        self.dx = 0#800 / 1000
+        self.dy = 1 / 1000
+        self.newX = 0
+        self.newY = 0
+        self.maxBounceAngle = math.pi / 12
 
-    def reset_ball(self, NUMBER, player1, player2):
-        if NUMBER == 0:
-            player1.score += 1
-            player1_score.text = '' + player1.score
-        elif NUMBER == 1:
-            player2.score += 1
-            player2_score.text = '' + player2.score
-        self.x = self.half_x
-        self.y = self.half_y
-        self.dx = -self.dx
-        self.dy = randint(-25, 25)
+# Class for the move
+class PAD:
+    def __init__(self):
+        self.midiOut = rtmidi.MidiOut()
+        self.midiIn = rtmidi.MidiIn()
 
-    def move(self, window):
-        self.x += self.dx
-        self.y += self.dy
+        if len(self.midiOut.ports) > 1:
+            self.midiOut.open_port(1)
+        else:
+            sys.exit("Pas d'appareil MIDI trouvé")
 
-        if self.x + self.width >= window.width:
-            self.reset_ball(0, window.players[0], window.players[1])
-        elif self.x <= 0:
-            self.reset_ball(1, window.players[0], window.players[1])
+        if len(self.midiIn.ports) > 0:
+            self.midiIn.open_port(0)
+        else:
+            sys.exit("Pas d'appareil MIDI trouvé")
 
-        if self.y < 0:
-            self.dy = -self.dy
-            self.y = 0
+    def reset(self):
+        """Methode qui d'arrêt complet du PAD"""
+        self.midiOut.send_message([176, 0, 0])
+        self.midiIn.close_port()
+        self.midiOut.close_port()
 
-        if self.y > window.height:
-            self.dy = -self.dy
-            self.y = window.height - self.height
-
+class Menu:
+    pass
 
 class App(pyglet.window.Window):
     def __init__(self):
         super(App, self).__init__(width=1920, height=1200, caption=TITLE + ' / ' + VERSION, resizable=False, fullscreen=True, vsync=True)
+        self.pad = PAD()
+        self.pad.midiIn.callback = self.callback
+
         self.looper = SourceGroup(music.audio_format, None)
         self.music_player = pyglet.media.Player()
         self.batch = Batch()
+        self.niveau = 3
 
         self.looper.loop = True
         self.looper.queue(music)
 
         self.music_player.volume = 1.0
         self.music_player.queue(self.looper)
-        self.music_player.play()
+        #self.music_player.play()
 
-        self.ball = Ball(ball, self.width // 2 - (ball.width // 2), self.height // 2- (ball.height // 2), batch=self.batch)
+        self.player1_score = Label('0', x=(self.width // 2) // 2 + 200, y=1100, font_size=60, font_name='Roboto Mono')
+        self.player2_score = Label('0', x=self.width // 2 + (self.width // 2) // 2 - 200, y=1100, font_size=60, font_name='Roboto Mono')
+
+        self.ball = Ball(ball, self.width // 2 - (ball.width // 2), self.height // 2 - (ball.height // 2), batch=self.batch)
         self.ball.half_x = self.width // 2 - (ball.width // 2)
         self.ball.half_y = self.height // 2 - (ball.height // 2)
-        self.players = [Player(0, paddle, 0, self.height // 2 - paddle.height // 2, self.batch), Player(1, paddle, self.width - paddle.width, self.height // 2 - paddle.height // 2, self.batch)]
+        self.players = [Player(0, paddle, 0, self.height // 2 - paddle.height // 2, self.batch, self), Player(1, paddle, self.width - paddle.width, self.height // 2 - paddle.height // 2, self.batch, self)]
 
         self.paused = False
 
         self.keys = key.KeyStateHandler()
-        pyglet.clock.schedule(self.update)
+        pyglet.clock.schedule_interval(self.update, 1 / 60)
 
-    def update(self,dt):
-        self.ball.move(self)
+    def level_1(self, message):
         pass
+
+    def level_2(self, message):
+        pass
+
+    def level_3(self, message):
+        nbrButton = 4
+        if message[0] == 144:
+            x = message[1] // 16
+            y = message[1] - (16 * x)
+
+            print(message, x, y, sep=' - ')
+
+            if y < 3:
+                self.players[0].x, self.players[0].y = self.players[0].positions[(x * nbrButton) + y]
+
+            if y > 4:
+                self.players[1].x, self.players[1].y = self.players[1].positions[(x * nbrButton) + nbrButton + (nbrButton - y) - 1]
+
+    def callback(self, message, dt):
+        if self.niveau == 1:
+            self.level_1(message)
+        elif self.niveau == 2:
+            self.level_2(message)
+        elif self.niveau == 3:
+            self.level_3(message)
+
+    def reset_ball(self, winner):
+        self.ball.x, self.ball.y = self.width / 2 - self.ball.width / 2, self.height / 2 - self.ball.height / 2
+        if winner == 1:
+            self.ball.dx = -(800 / 1000)
+        else:
+            self.ball.dx = 800 / 1000
+        self.ball.dy = 1 / 1000
+
+        self.reset_paddle()
+
+    def reset_paddle(self):
+        for p in self.players:
+            if p.NUMBER == 0:
+                p.x, p.y = 0, self.height // 2 - p.height // 2
+            else:
+                p.x, p.y = self.width - p.width, self.height // 2 - p.height // 2
+
+
+    def update(self, dt):
+        dt *= 1000
+        b = self.ball
+        b.newX = b.x + b.dx * dt
+        b.newY = b.y + b.dy * dt
+
+        paddle1 = self.players[0]
+        paddle2 = self.players[1]
+
+        if b.newY < 0:
+            b.newY = -b.newY
+            b.dy = -b.dy
+        elif b.newY + b.width > self.height:
+            b.newY -= 2 * ((b.newY + b.width) - self.height)
+            b.dy = -b.dy
+
+        if b.newX < paddle1.x + paddle1.width <= b.x:
+            intersectX = paddle1.x + paddle1.width
+            intersectY = b.y - ((b.x - (paddle1.x + paddle1.width)) * (b.y - b.newY)) / (b.x - b.newX)
+            if paddle1.y <= intersectY <= paddle1.y + paddle1.height:
+                relativeIntersectY = (paddle1.y + (paddle1.height / 2)) - intersectY
+                bounceAngle = (relativeIntersectY / (paddle1.height / 2)) * (math.pi / 2 - b.maxBounceAngle)
+                ballSpeed = math.sqrt(b.dx * b.dx + b.dy * b.dy)
+                ballTravelLeft = (b.newY - intersectY) / (b.newY - b.y)
+                b.dx = ballSpeed * math.cos(bounceAngle)
+                b.dy = ballSpeed * -math.sin(bounceAngle)
+                b.newX = intersectX + ballTravelLeft * ballSpeed * math.cos(bounceAngle)
+                b.newY = intersectY + ballTravelLeft * ballSpeed * math.sin(bounceAngle)
+
+                b.dx += 0.01
+
+        if b.newX > paddle2.x - paddle2.width >= b.x:
+            intersectX = paddle2.x - paddle2.width
+            intersectY = b.y - ((b.x - (paddle2.x - paddle2.width)) * (b.y - b.newY)) / (b.x - b.newX)
+            if paddle2.y <= intersectY <= paddle2.y + paddle2.height:
+                relativeIntersectY = (paddle2.y + (paddle2.height / 2)) - intersectY
+                bounceAngle = (relativeIntersectY / (paddle2.height / 2)) * (math.pi / 2 - b.maxBounceAngle)
+                ballSpeed = math.sqrt(b.dx * b.dx + b.dy * b.dy)
+                ballTravelLeft = (b.newY - intersectY) / (b.newY - b.y)
+                b.dx = ballSpeed * math.cos(bounceAngle) * -1
+                b.dy = ballSpeed * math.sin(bounceAngle) * -1
+                b.newX = intersectX - ballTravelLeft * ballSpeed * math.cos(bounceAngle)
+                b.newY = intersectY - ballTravelLeft * ballSpeed * math.sin(bounceAngle)
+
+                b.dx -= 0.01
+
+        if b.newX < 0:
+            paddle2.score += 1
+            self.reset_ball(0)
+            return
+        elif b.newX + b.width > self.width + 100:
+            paddle1.score += 1
+            self.reset_ball(1)
+            return
+
+        self.player1_score.text = str(self.players[0].score)
+        self.player2_score.text = str(self.players[1].score)
+
+        b.x = b.newX
+        b.y = b.newY
 
     def on_draw(self):
         window.clear()
         background.blit(0, 0)
+        self.player1_score.draw()
+        self.player2_score.draw()
         self.batch.draw()
-        player1_score.draw()
-        player2_score.draw()
 
 
 if __name__ == '__main__':
@@ -123,210 +245,4 @@ if __name__ == '__main__':
     window.set_exclusive_mouse()
     window.set_icon(icon)
     pyglet.app.run()
-
-
-# Class for the PAD
-
-    # # Class logic of the Game
-    # class Ball(Move):
-    #     def step(self, dt):
-    #         super(Ball, self).step(dt)
-    #
-    #         velocity_x = self.target.max_speed * 10
-    #         velocity_y = 0  # self.target.max_speed * 4
-    #
-    #         self.target.velocity = (velocity_x, velocity_y)
-    #
-    #
-    # # Class of the layer
-    # class BackgroundLayer(Layer):
-    #     def __init__(self, name=''):
-    #         super(BackgroundLayer, self).__init__()
-    #         self.pseudo = name
-    #         self.img = pyglet.resource.image('background.jpg')
-    #
-    #     def draw(self):
-    #         glPushMatrix()
-    #         self.transform()
-    #         self.img.blit(0, 0)
-    #         glPopMatrix()
-    #
-    #
-    # class GameLayer(Layer):
-    #     is_event_handler = True
-    #
-    #     def __init__(self):
-    #         w, h = director.get_window_size()
-    #         super(GameLayer, self).__init__()
-    #
-    #         # Create the Sprite
-    #         self.player1 = Sprite(pyglet.resource.image('paddle.png'))
-    #         self.player2 = Sprite(pyglet.resource.image('paddle.png'))
-    #         self.ball = Sprite(pyglet.resource.image('ball.png'))
-    #
-    #         # Add cshape property to Sprite
-    #         self.player1.cshape = cm.AARectShape(self.player1.position, self.player1.width // 2, self.player1.height // 2)
-    #         self.player2.cshape = cm.AARectShape(self.player2.position, self.player2.width // 2, self.player2.height // 2)
-    #         self.ball.cshape = cm.CircleShape(self.ball.position, 5)
-    #
-    #         # Add a collison manager
-    #         self.collisionManager = cm.CollisionManagerBruteForce()
-    #         self.collisionManager.add(self.ball)
-    #         self.collisionManager.add(self.player1)
-    #         self.collisionManager.add(self.player2)
-    #
-    #         # Position the sprite
-    #         self.player1.position = 0 + self.player1.width, (h // 2) + (self.player1.height // 2)
-    #         self.player2.position = w - self.player2.width, (h // 2) + (self.player2.height // 2)
-    #         self.ball.position = (w // 2) - (self.ball.width / 2), (h // 2) + (self.ball.height // 2)
-    #
-    #         # Init the player
-    #         self.player1.positions = np.array([[Player(x, y) for y in range(0, 3)] for x in range(8)])
-    #         self.player2.positions = np.array([[Player(x, y) for y in range(4, 8)] for x in range(8)])
-    #
-    #         # Init the ball
-    #         self.ball.max_speed = 35
-    #         self.ball.velocity = (0, 0)
-    #
-    #         # Add the Sprite to the Layer
-    #         self.add(self.player1)
-    #         self.add(self.player2)
-    #         self.add(self.ball)
-    #
-    #         self.ball.do(Move())
-    #         self.schedule(self.update)
-    #
-    #     def update(self, dt):
-    #         self.ball.cshape.center = self.ball.position
-    #         self.player1.cshape.center = self.player1.position
-    #         self.player2.cshape.center = self.player2.position
-    #
-    #         for obj in self.collisionManager.objs:
-    #             print(obj)
-    #
-    #         collisions = self.collisionManager.objs_colliding(self.ball)
-    #         if collisions:
-    #             if self.player1 in collisions:
-    #                 print('Player 1 --> Ball')
-    #             elif self.player2 in collisions:
-    #                 print('Player 2 --> Ball')
-    #
-    #     def on_quit(self):
-    #         director.push(FlipAngular3DTransition(MenuScene(), duration=3))
-    #
-    #     def on_key_press(self, symbol, modifiers):
-    #         if symbol == key.F1:
-    #             self.on_quit()
-    #
-    #
-    # # Class menu
-    # class MainMenu(Menu):
-    #     def __init__(self):
-    #         super(MainMenu, self).__init__('Pong - PAD')
-    #
-    #         self.font_title['font_name'] = 'TRACEROUTE'
-    #         self.font_title['font_size'] = 200
-    #         self.font_title['color'] = (255, 10, 10, 255)
-    #
-    #         self.font_item['font_name'] = 'TRACEROUTE'
-    #         self.font_item['font_size'] = 60
-    #         self.font_item['color'] = (110, 110, 110, 255)
-    #
-    #         self.font_item_selected['font_name'] = 'TRACEROUTE'
-    #         self.font_item_selected['font_size'] = 65
-    #         self.font_item_selected['color'] = (110, 110, 110, 255)
-    #
-    #         self.menu_halign = CENTER
-    #         self.menu_valign = CENTER
-    #
-    #         items = []
-    #         items.append(MenuItem('Nouvelle partie', self.on_newGame))
-    #         items.append(MenuItem('Options', self.on_options))
-    #         items.append(MenuItem('Quitter', self.on_quit))
-    #
-    #         self.create_menu(items, zoom_in(), zoom_out())
-    #
-    #     def on_newGame(self):
-    #         director.push(FlipAngular3DTransition(GameScene(), duration=3))
-    #
-    #     def on_options(self):
-    #         director.push(FlipAngular3DTransition(OptionsScene(), duration=3))
-    #
-    #     def on_quit(self):
-    #         sys.exit()
-    #
-    #
-    # class OptionsMenu(Menu):
-    #     is_event_handler = True
-    #
-    #     def __init__(self):
-    #         super(OptionsMenu, self).__init__('Options')
-    #
-    #         self.font_title['font_name'] = 'TRACEROUTE'
-    #         self.font_title['font_size'] = 140
-    #         self.font_title['color'] = (255, 10, 10, 255)
-    #
-    #         self.font_item['font_name'] = 'TRACEROUTE'
-    #         self.font_item['font_size'] = 50
-    #         self.font_item['color'] = (110, 110, 110, 255)
-    #
-    #         self.font_item_selected['font_name'] = 'TRACEROUTE'
-    #         self.font_item_selected['font_size'] = 55
-    #         self.font_item_selected['color'] = (110, 110, 110, 255)
-    #
-    #         items = []
-    #         self.volumes = ['Mute', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100']
-    #
-    #         items.append(MultipleMenuItem('Volume de la musique : ', self.on_music_volume, self.volumes,
-    #                                       int(music_player.volume * 10)))
-    #         items.append(ToggleMenuItem('Afficher les FPS : ', self.on_show_fps, director.show_FPS))
-    #         items.append(MenuItem('Retour', self.on_quit))
-    #
-    #         self.create_menu(items, zoom_in(), zoom_out())
-    #
-    #     def on_quit(self):
-    #         director.push(FlipAngular3DTransition(MenuScene(), duration=3))
-    #
-    #     def on_show_fps(self, value):
-    #         director.show_FPS = value
-    #
-    #     def on_music_volume(self, idx):
-    #         vol = idx / 10.0
-    #         music_player.volume = vol
-    #
-    #
-    # # Class for the Scene
-    # class IntroScene(Scene):
-    #     def __init__(self, *children):
-    #         super().__init__(*children)
-    #         self.add(BackgroundLayer(), z=0)
-    #
-    #
-    # class MenuScene(Scene):
-    #     def __init__(self, *children):
-    #         super(MenuScene, self).__init__(*children)
-    #         self.add(BackgroundLayer(), z=0)
-    #         self.add(MainMenu(), z=1)
-    #
-    #
-    # class GameScene(Scene):
-    #     def __init__(self, *children):
-    #         super(GameScene, self).__init__(*children)
-    #         self.add(BackgroundLayer(), z=0)
-    #         self.add(GameLayer(), z=1)
-    #
-    #
-    # class OptionsScene(Scene):
-    #     def __init__(self, *children):
-    #         super(OptionsScene, self).__init__(*children)
-    #         self.add(BackgroundLayer(), z=0)
-    #         self.add(OptionsMenu(), z=1)
-
-    # director.init(width=1920, height=1200, caption=TITLE + ' / ' + VERSION, fullscreen=False, resizable=True,
-    #              vsync=True)
-    # director.window.set_exclusive_mouse(True)
-    # menu = MenuScene()
-
-    # director.window.push_handlers(keys)
-
-    # director.run(menu)
+    window.pad.reset()
